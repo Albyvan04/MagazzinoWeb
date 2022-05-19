376662c7d2b089c7af1d9201e3a0350af0f873b4
Andrea Piola <93667356+SigX@users.noreply.github.com>
2022-05-19 11:57:29 +0200

    Add files via upload

--- Inserimento.php
+++ Inserimento.php
@@ -24,23 +24,23 @@
 <body class="sfondo">
     <h1 style="color: aliceblue;">Form di inserimento prodotti</h1>    
     
-    <div class="card mb-3" style="max-width: 800px;margin-top: 10%; margin-left: 30%; margin-right:30%; margin-bottom:10%">
+    <div class="card mb-3" style="max-width: 1000px;margin-top: 6%; margin-left: 30%; margin-right:30%; margin-bottom:10%">
         <div class="row g-0" style="align-items: center;">
           <div class="col-md-4">
             <img src="account.jpg" class="img-fluid rounded-start" style="height: 250px; width: 200px;">
           </div>
           <div class="col-md-8">
             <form>
-                <br><br><label>Descrizione:</label><br>
-                <input type="text" style="max-width: 400px; margin-left: 10%; margin-right:10%;" class="form-control" placeholder="Descrizione"  aria-describedby="addon-wrapping" name="descrizione"><br>
-                <label>Prezzo:</label><br>
-                <div class="input-group mb-3" style="margin-left: 10%; margin-right:10%;">
+                <br><br><label style="margin-left: 20%">Descrizione:</label><br>
+                <input type="text" style="max-width: 250px; margin-left: 20%; margin-right:10%;" class="form-control" placeholder="Descrizione"  aria-describedby="addon-wrapping" name="descrizione" maxlength="20"><br>
+                <label style="margin-left: 20%">Prezzo:</label><br>
+                <div class="input-group mb-3" style="margin-left: 20%; margin-right:10%;">
                     <span class="input-group-text" style=" max-width: 50px;" name="prezzo">€</span>
-                    <input type="text" style="max-width: 370px;" class="form-control" aria-label="Amount (to the nearest dollar)">
+                    <input type="text" style="max-width: 220px;" class="form-control" onkeypress="return (event.charCode >= 48 && event.charCode <= 57) || event.charCode == 44 || event.charCode == 0" min="0,0" max="9999,00" maxlength="6">
                 </div><br>
                 <div>
-                    <label style="margin-left: -50%;">Quantità:</label><br>
-                    <input type="number" name="quantità" value="5" min="0">
+                    <label style="margin-left: -40%;">Quantità:</label><br>
+                    <input type="number" name="quantità" onkeypress="return (event.charCode >= 48 && event.charCode <= 57) || event.charCode == 0" min="0" value="5"><br><br>
                     <input type="submit" style="background-color:cadetblue; margin-left: 10%;" class="btn btn-primary btn-lg" value="Inserisci" name="Inserisci">
                 </div>
                 </form>
