9b728e9ab3bffce4f85cf103fed598d624fad799
Andrea Piola <93667356+SigX@users.noreply.github.com>
2022-05-19 11:35:19 +0200

    Add files via upload

--- Magazzino.php
+++ Magazzino.php
@@ -34,24 +34,24 @@
 </head>
 
 <body class="sfondo">
-    <nav class="navbar" style="background-color: transparent;">
+    <nav class="navbar sticky-top bg-ligh" style="background-color: darkgray;">
     <div class="container-fluid">
         <a class="navbar-brand"><img src="logo.jpg" style="height: 40px; width:80px;"></a>
-        <button type="button" class="btn btn-outline-success" name="aggiungi" style="margin-left: -60%;">Aggiungi</button>
+        <button type="button" class="btn btn-outline-success" name="aggiungi" style="margin-left: -40%;">Aggiungi</button>
         <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Cerca" aria-label="Search" style="width: 400px;">
-        <button class="btn btn-outline-success" type="submit">Cerca</button>
+        <input class="form-control me-2" type="search" placeholder="Cerca" aria-label="Search" style="width: 400px;" name="txtCerca">
+        <button class="btn btn-outline-primary" type="submit" name="cerca">Cerca</button>
         </form>
     </div>
     </nav>
-    <table class="table table-sm">
+    <table class="table table-sm" style="border:transparent;">
         <?php 
-            $numero = count($elementi)/2;
+            $numero = count($elementi)/3;
             for($i = 0; $i < $numero; $i++)
             {
-                echo ("<TR>");
+                echo ("<TR>);
                 echo("<td>
-                <div class='card' style='width: 18rem;'>
+                <div class='card' style='width: 18rem; margin-left:7%;display: inline-block;'>
                     <div class='card-body'>
                         <h5 class='card-title'>".$elementi[$i]->getDescription()."</h5>
                         <ul class='list-group list-group-flush'>
@@ -62,9 +62,8 @@
                         <button type="button" class="btn btn-danger" name="elimina" style="width:50px; margin-left:10%;">X</button>
                     </div>
                 </div>
-                </td>");
-                echo ("</TR>");      
-            }
+                echo("</td>");
+                echo ("</TR>");
         ?>
     </table>
 </body>
